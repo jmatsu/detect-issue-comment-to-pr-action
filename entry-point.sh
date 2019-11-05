f981dab9fae27a34326d1a86fb702d77142b9427
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-11-05 14:39:16 +0900

    Use Issue#pull_request attributes

--- entry-point.sh
+++ entry-point.sh
@@ -5,20 +5,12 @@ set -o pipefail
 
 source /toolkit.sh
 
-cat $GITHUB_EVENT_PATH | github::debug
-
-readonly issue_number="$(cat $GITHUB_EVENT_PATH | jq -r '.issue_comment.html_url' | sed 's/^.*\/issues\/\([0-9]*\)#.*$/\1/')"
-
-if [[ -z $(curl -X GET \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$issue_number" | \
-  jq -r '.pull_request // empty') ]]; then
+if [[ -z "$(cat $GITHUB_EVENT_PATH | jq -r '.issue.pull_request // empty')" ]]; then
   github::debug "Commented to non pull request"
   github::set_output "is_pull_request" "false"
   github::success # does not use a failure status
 fi
 
-github::set_output "pull_number" "$issue_number"
+github::set_output "pull_number" "$(cat $GITHUB_EVENT_PATH | jq -r '.issue.number')"
 github::set_output "is_pull_request" "true"
 github::success
